45991f4d33836212de5788e246a0b2d9b5500e83
Abit25 <abheet25@gmail.com>
2019-12-08 22:33:37 +0530

    Final Changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,27 @@
 console.log("Client Side JS Loaded");
 
 const form = document.getElementById("form");
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   const loc = document.getElementById("loc");
-
+  p1.textContent = "Loading....";
+  p2.textContent = "";
   fetch("/weather?address=" + loc.value).then(response => {
     response.json().then(data => {
-      console.log("Data ", data.error);
+      if (data.error) {
+        p1.textContent = "Error : " + data.error.error;
+      } else {
+        p1.textContent = data.location;
+        p2.textContent =
+          "The average temperature for the day is " +
+          data.currentTemp +
+          " C . The relative humidity is at " +
+          data.humidity * 100 +
+          "%";
+      }
     });
   });
 });
